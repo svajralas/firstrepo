035795bd7ab21f3ec933f106b162d3aeeff323cb
svajralas <53432805+svajralas@users.noreply.github.com>
2019-08-16 18:06:59 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<10; i++){
+			System.out.println("Hello World to Jenkins "+i);
+		}
+		
+
+	}
+
+}
