c711bd46ac853c814fcf8ee3283f367de1a43239
svajralas <53432805+svajralas@users.noreply.github.com>
2019-08-16 18:11:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		for(int i=0; i<10; i++){
+		for(int i=0; i<100; i++){
 			System.out.println("Hello World to Jenkins "+i);
 		}
 		
